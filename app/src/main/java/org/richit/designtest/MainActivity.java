ff418878e31b030a61e2629a76babba731ac4d34
plabon <shuvadey8@gmail.com>
2019-08-21 23:07:45 +0600

    xenon

--- app/src/main/java/org/richit/designtest/MainActivity.java
+++ app/src/main/java/org/richit/designtest/MainActivity.java
@@ -1,6 +1,5 @@
 package org.richit.designtest;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,9 +7,11 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView1 ;
+    TextView textView ;
     Button buttonClick;
 
 
@@ -18,14 +19,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView1 = findViewById(R.id.text);
+        textView = findViewById(R.id.text);
         buttonClick = findViewById(R.id.button);
         buttonClick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textView1.setTextColor(Color.RED);
-                textView
+                textView.setText(generateString(6));
+
             }
         });
     }
+    private String generateString (int length){
+        char[] chars ="ABCDEFGHIJKLMNOPQRST123456789".toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+        Random random = new Random();
+        for(int i = 0;i < length; i++){
+            char c = chars[random.nextInt(chars.length)];
+            stringBuilder.append(c);
+        }
+        return stringBuilder.toString();
+    }
+
+
 }
