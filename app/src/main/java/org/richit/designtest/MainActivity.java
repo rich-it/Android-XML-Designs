9ad81bcbc6957b41465cd36a42c6760a40b86135
plabon <shuvadey8@gmail.com>
2019-08-21 17:10:59 +0600

    xenon

--- app/src/main/java/org/richit/designtest/MainActivity.java
+++ app/src/main/java/org/richit/designtest/MainActivity.java
@@ -1,14 +1,31 @@
 package org.richit.designtest;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView1 ;
+    Button buttonClick;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView1 = findViewById(R.id.text);
+        buttonClick = findViewById(R.id.button);
+        buttonClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView1.setTextColor(Color.RED);
+                textView
+            }
+        });
     }
 }
