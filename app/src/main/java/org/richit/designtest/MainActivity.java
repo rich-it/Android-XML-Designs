35395ca76b7d69315d1543c5a90c85118bf52c0b
Arfatul Mowla <mowlashuvo2@gmail.com>
2019-08-21 23:21:28 -0700

    fixedrandomtext

--- app/src/main/java/org/richit/designtest/MainActivity.java
+++ app/src/main/java/org/richit/designtest/MainActivity.java
@@ -11,19 +11,20 @@ public class MainActivity extends AppCompatActivity {
 
     TextView textViewPerson;
     Button button;
-    String nameArray [] = {"Shuvo, Masrur, Fayaz, Plabon"};
+    String nameArray[] = {"Shuvo, Masrur, Fayaz, Plabon"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textViewPerson = findViewById(R.id.personviewtv);
+        int i;
         button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (i=nameArray.length-1)
-                    i=0;
+                if (i = nameArray.length - 1)
+                    i = 0;
                 textViewPerson.setText(nameArray[i++]);
             }
         });
