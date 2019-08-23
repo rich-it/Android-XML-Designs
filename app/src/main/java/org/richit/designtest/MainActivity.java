b4e4c5c6876ff85ed92fcaebcac7488d3ae13752
Dany Barua <danybarua5@gmail.com>
2019-08-24 03:48:29 +0600

    adding design and task

--- app/src/main/java/org/richit/designtest/MainActivity.java
+++ app/src/main/java/org/richit/designtest/MainActivity.java
@@ -1,14 +1,39 @@
 package org.richit.designtest;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    TextView text1;
+    Button button1;
+    String array[] = {"This","is","a","completely","random","text","generated","from","an","array"};
+    int i=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text1 = findViewById(R.id.text1);
+        button1 = findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text1.setText(array[i]);
+                i++;
+                if(i==10)
+                    i=0;
+
+            }
+        });
     }
+
+
+
 }
